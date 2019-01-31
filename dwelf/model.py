fixed log-likelihood function

dwelf/model.py
@@ -13,12 +13,13 @@ from . import MPLSTYLE
 
 
 class MaculaModeler(object):
-    def __init__(self, t, y, nspots, inc=None, Peq=None, k2=None, k4=None, c1=None, c2=None, c3=None, c4=None,
+    def __init__(self, t, y, nspots, dy=None, inc=None, Peq=None, k2=None, k4=None, c1=None, c2=None, c3=None, c4=None,
                  d1=None, d2=None, d3=None, d4=None, long=None, lat=None, alpha=None, fspot=None, tmax=None,
                  life=None, ingress=None, egress=None, U=None, B=None, tstart=None, tend=None):
         self.t = t
         self.y = y
         self.nspots = nspots
+        self.dy = dy
         self.inc = inc
         self.Peq = Peq
         self.k2 = k2
@@ -43,6 +44,9 @@ class MaculaModeler(object):
         self.B = B
         self.tstart = tstart
         self.tend = tend
+
+        if self.dy is None:
+            self.dy = np.ones_like(y)
 
         if self.inc is None:
             self.inc = np.array([0, np.pi/2])
@@ -176,7 +180,7 @@ class MaculaModeler(object):
         theta_spot = np.array([theta_full[key] for key in self.spot_pars.keys()])
         theta_inst = np.array([theta_full[key] for key in self.inst_pars.keys()])
         yf = macula(self.t, theta_star, theta_spot, theta_inst, tstart=self.tstart, tend=self.tend)
-        sse = np.sum(np.square(yf - self.y)) / np.std(self.y)
+        sse = np.sum(np.square(yf - self.y)) / np.std(self.y) ** 2
         return sse
 
     def lnprob(self, theta):
@@ -255,7 +259,9 @@ class MaculaModeler(object):
             return x
 
         def logl(cube):
-            return -self.chi(cube)
+            n = self.t.size
+            c = - .5 * n * np.log(2 * np.pi) - .5 * n * np.log(np.std(self.y))
+            return c - .5 * self.chi(cube)
 
         ndim = self.bounds.shape[0]
 